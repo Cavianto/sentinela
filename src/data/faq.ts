3f3986d232912a9ebfdbb76d48f828007fc48868
Cavianto <hello@cavianto.com>
2025-05-25 19:50:40 +1000

    Update faq.ts

--- src/data/faq.ts
+++ src/data/faq.ts
@@ -16,7 +16,7 @@ export const faqs: IFAQ[] = [
     },
     {
         question: 'How does the process of engaging with Zentinela work?',
-        answer: '1) Select Your Service: Choose an investigation service from our website. You’ll then be redirected to provide your contact details and case specifics. 2) Schedule a Consultation (Optional): After submitting your request, we’ll send a video conference link or phone number (per your preference). We’ll contact you: - Same day if submitted during business hours (8:00 AM – 3:00 PM Melbourne time). - Next business day for after-hours inquiries. - Prefer email-only communication? Simply let us know. 3) Receive Your Proposal: We’ll email a detailed breakdown including: - Investigation scope - Estimated timeframe - Service agreement - Transparent pricing 4) Approve & Pay: Sign the agreement and complete payment to initiate your investigation. - We accept BTC payments 5) Get Your Report: Receive a comprehensive OSINT report tailored to your selected service.',
+        answer: 'Yes',
     },
     {
         question: 'What if I need help using the app?',
