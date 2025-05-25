b62ea703cd5ee3e9a6ae80931863afd83f5c6877
Cavianto <hello@cavianto.com>
2025-05-25 15:04:17 +1000

    Update PricingColumn.tsx

--- src/components/Pricing/PricingColumn.tsx
+++ src/components/Pricing/PricingColumn.tsx
@@ -19,7 +19,7 @@ const PricingColumn: React.FC<Props> = ({ tier, highlight }: Props) => {
                     <span className={clsx({ "text-secondary": highlight })}>
                         {typeof price === 'number' ? `$${price}` : price}
                     </span>
-                    {typeof price === 'number' && <span className="text-lg font-normal text-gray-600">/mo</span>}
+                    {typeof price === 'number' && <span className="text-lg font-normal text-gray-600">/Single Payment</span>}
                 </p>
                 <button className={clsx("w-full py-3 px-4 rounded-full transition-colors", { "bg-primary hover:bg-primary-accent": highlight, "bg-hero-background hover:bg-gray-200": !highlight })}>
                     Get Started
