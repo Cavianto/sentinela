c0dfd3957abb80ccba9d3439e21c1fa2fb49444a
Cavianto <hello@cavianto.com>
2025-05-28 13:21:55 +1000

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import Image from 'next/image';
 
-import AppStoreButton from './AppStoreButton';
-import PlayStoreButton from './PlayStoreButton';
+// import AppStoreButton from './AppStoreButton';
+// import PlayStoreButton from './PlayStoreButton';
 
 import { heroDetails } from '@/data/hero';
 
@@ -23,9 +23,10 @@ const Hero: React.FC = () => {
             <div className="text-center">
                 <h1 className="text-4xl md:text-6xl md:leading-tight font-bold text-foreground max-w-lg md:max-w-2xl mx-auto">{heroDetails.heading}</h1>
                 <p className="mt-4 text-foreground max-w-lg mx-auto">{heroDetails.subheading}</p>
+                {/* Removed AppStore and PlayStore buttons */}
                 <div className="mt-6 flex flex-col sm:flex-row items-center sm:gap-4 w-fit mx-auto">
-                    <AppStoreButton dark />
-                    <PlayStoreButton dark />
+                    {/* <AppStoreButton dark /> */}
+                    {/* <PlayStoreButton dark /> */}
                 </div>
                 <Image
                     src={heroDetails.centerImageSrc}
