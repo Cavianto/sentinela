dfd235108cf2c6015fbee90fc20c6e7724f4e10d
Cavianto <hello@cavianto.com>
2025-05-28 13:04:44 +1000

    Update CTA.tsx

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -17,7 +17,6 @@ const CTA: React.FC = () => {
                         <p className="mx-auto max-w-xl md:px-5">{ctaDetails.subheading}</p>
 
                         <div className="mt-4 flex flex-col sm:flex-row items-center sm:gap-4">
-                        <AppStoreButton />
                         <PlayStoreButton />
                         </div>
                     </div>
